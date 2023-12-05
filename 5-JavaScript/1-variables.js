d05dbdccbb8ddba5aee23d83457f408c69fa2a93
Kgonlez <test2@gmail.com>
2023-12-04 22:14:37 -0800

    Added JavaScript files

---
+++ 5-JavaScript/1-variables.js
@@ -0,0 +1,35 @@
+console.log("Hello World!");
+
+//This is how we comment a single line of code
+/* This is how we comment a multiple 
+lines of codes */
+
+//variables
+
+//Let
+let name = "John";
+console.log("This is the name of the user:", name);
+
+let firstName = "John";
+let lastName = "Doe";
+
+console.log("This is the name of the user:", firstName, lastName);
+
+//Var
+var myFavoriteGame = "Animal Crossing";
+console.log("This is my favorite game:", myFavoriteGame);
+
+//Comparison Let vs Var
+if (true) {
+  let myFavoriteDrink = "Cafe atte";
+  var mySecondFavorite = "Tea";
+
+  console.log(myFavoriteDrink);
+}
+
+console.log(mySecondFavorite);
+
+//Const
+const gravity = '9.8';
+const birthday =" November 16"
+//gravity = "9.5";
---
+++ 5-JavaScript/app.js
@@ -0,0 +1,21 @@
+//string - abc123!@#$
+let name = "John";
+console.log("Type of data type:", typeof name);
+
+//Number -12345
+let age = 25;
+console.log("Type of data type:", typeof age);
+
+//Boolean - true or false
+let isStudent = false;
+let isTeacher = true;
+console.log("Type of data type:", typeof isStudent);
+console.log("Type of data type:", typeof isTeacher);
+
+//Null - Intentional abscence of data
+let girlFriend = null;
+console.log("Type of data type:", girlFriend);
+
+//Undefined
+let city;
+console.log("Type of data type:", city);
